a3cc23e4e38b43c74727fbf0119ad3f85a4166a9
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2020-11-27 16:22:18 +0400

    saas

--- Bloknot2/MainWindow.xaml.cs
+++ Bloknot2/MainWindow.xaml.cs
@@ -22,6 +22,9 @@ namespace Bloknot2
     /// </summary>
     public partial class MainWindow : Window ,IMainWindow
     {
+        public string FilePath => throw new NotImplementedException();
+
+        string IMainWindow.Content { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public event EventHandler FileOpen;
         public event EventHandler FileSave;
@@ -31,91 +34,11 @@ namespace Bloknot2
         public MainWindow()
         {
             InitializeComponent();
-            btOpen.Click += BtOpen_Click;
-            btSave.Click += BtSave_Click;
-            tbContent.TextChanged += MainWindow_ContentChane;
-            slSize.ValueChanged += SlSize_ValueChanged;
-
-            btSelect.Click += BtSelect_Click;
-
-
-            WindowCollection windowCollection = App.Current.Windows;
-
-
-            FileManager fileMabeger = new FileManager();
-            MessageServis messageServis = new MessageServis();
-             MyPresenter myPresenter = new MyPresenter(messageServis, this , fileMabeger);
-
-        }
-
-        private void MainWindow_ContentChane(object sender, EventArgs e)
-        {
-            if (ContentChane != null)
-            {
-               ContentChane(this, EventArgs.Empty);
-            }
-        }
-
-        private void BtSave_Click(object sender, RoutedEventArgs e)
-        {
-            if (FileSave!=null)
-            {
-                FileSave(this, EventArgs.Empty);
-            }
-        }
-
-        private void BtOpen_Click(object sender, RoutedEventArgs e)
-        {
-            tbContent.Document = new FlowDocument();
-
-            if (FileOpen != null) 
-            {
-                FileOpen(this, EventArgs.Empty); 
-            }
-        }
-
-        public string FilePath
-        {
-            get
-            {
-                return tbFilePath.Text;
-            }
-        }
-
-        string IMainWindow.Content 
-        { 
-            get { return new TextRange(tbContent.Document.ContentStart, tbContent.Document.ContentEnd).Text; }
-            set { tbContent.AppendText(value); }
         }
 
         public void SetSymbolCount(int count)
         {
-            lbCountSimbol.Content = "Колл-во  символов " +  count.ToString();
-        }
-
-        /// <summary>
-        /// Открывает  окно диалга для выбора файла
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void BtSelect_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog(); //  using Microsoft.Win32; // Эту бибилиотеку  надо
-
-           if  (  dialog.ShowDialog()== true)
-            {
-                tbFilePath.Text = dialog.FileName;
-            }
-        }
-
-        /// <summary>
-        /// Меняет  размер  шрифта контента
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void SlSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            tbContent.FontSize = slSize.Value;
+            throw new NotImplementedException();
         }
     }
 }
